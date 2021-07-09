1cb46c5e6bd0367f433c977875445e1a5e7e3a40
cmbuffa <cmbuffa@gmail.com>
2021-07-09 10:58:52 -0400

    GenericXMLSerializer Test

---
+++ TestPingTCP/GenericXmlSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TestPingTCP
+{
+    class GenericXmlSerializer
+    {
+        public T Xml<T>(string xml, out string Error) where T : new()
+        {
+            T comprobante = new T();
+
+            Error = "";
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                StringReader sXML = new StringReader(xml);
+                comprobante = (T)serializer.Deserialize(sXML);
+
+            }
+            catch (Exception ex)
+            {
+                Error = "Error en la Serialización" + ex.Message;
+                comprobante = new T();
+            }
+            return comprobante;
+
+        }
+    }
+}
